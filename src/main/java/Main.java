493b3d5ba4a6a9f38db53d897c608218ccd05267
prathik-poojary <prathikpoojary359@gmail.com>
2026-06-20 22:02:28 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,191 +1,172 @@
-import static java.lang.IO.readln;
+import command.Command;
+import command.CommandFactory;
+import command.CommandOutput;
+import command.functions.Exit;
 
-Set<String> BUILTINS = Set.of("cd", "echo", "exit", "pwd", "type");
+import state.CurrentPath;
 
-void main() {
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
-    // noinspection InfiniteLoopStatement
-    while (true) {
-        final String input = readln("$ ");
-        final String[] tokens = tokenize(input);
-        if (tokens.length == 0)
-            continue;
+public class Main {
+    static void main(String[] args) throws Exception {
+        CommandFactory commandFactory = new CommandFactory();
+        boolean run = true;
+        while (run) {
+            System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
+            String commandLine = scanner.nextLine();
 
-        var stdout = Optional.<String>empty();
-        var stderr = Optional.<String>empty();
+            String[] arguments;
 
-        int i = 1;
-        while (stdout.isEmpty() && i < tokens.length - 1) {
-            switch (tokens[i++]) {
-                case "1>", ">" -> stdout = Optional.of(tokens[i]);
-                case "2>" -> stderr = Optional.of(tokens[i]);
-            }
-        }
+            boolean inSingleQuotes = false;
+            boolean inDoubleQuotes = false;
 
-        var output = handle(Arrays.copyOfRange(tokens, 0,
-                stdout.isPresent() || stderr.isPresent()
-                        ? i - 1
-                        : tokens.length));
+            boolean isEscaped = false;
+            boolean isEscapedConditionally = false;
+            List<String> argsList = new ArrayList<>();
 
-        BiConsumer<Optional<String>, String> lambda = (o,
-                str) -> o.ifPresentOrElse(
-                        fs -> {
-                            try {
-                                var path = Paths.get(fs);
-                                Files.createDirectories(path.getParent());
-                                try (var fw = Files.newBufferedWriter(path,
-                                        StandardCharsets.UTF_8)) {
-                                    // Overwrites the file
-                                    fw.write(str);
-                                }
-                            } catch (IOException e) {
-                                e.printStackTrace(System.out);
-                            }
-                        },
-                        () -> System.out.append(str));
+            StringBuilder currentArg = new StringBuilder();
 
-        lambda.accept(stdout, output.std());
-        lambda.accept(stderr, output.err());
-    }
-}
+            for (char current : commandLine.toCharArray()) {
+                if (isEscaped) {
+                    currentArg.append(current);
+                    isEscaped = false;
+                    continue;
+                }
 
-Output handle(String[] tokens) {
-    try (var outStream = new ByteArrayOutputStream();
-            var errStream = new ByteArrayOutputStream();
-            var out = new PrintStream(outStream);
-            var err = new PrintStream(errStream)) {
+                if (isEscapedConditionally) {
+                    switch (current) {
+                        case '"', '\\':
+                            currentArg.append(current);
+                            break;
+                        default:
+                            currentArg.append('\\').append(current);
+                    }
+                    isEscapedConditionally = false;
+                    continue;
+                }
 
-        switch (tokens[0]) {
-            case "exit" -> System.exit(0);
-            case "echo" -> {
-                if (tokens.length != 1) {
-                    int i = 1;
-                    while (i < tokens.length - 1)
-                        out.print(tokens[i++] + " ");
-                    out.println(tokens[i]);
+                switch (current) {
+                    case '\'':
+                        if (inDoubleQuotes) {
+                            currentArg.append(current);
+                        } else {
+                            inSingleQuotes = !inSingleQuotes;
+                        }
+                        break;
+                    case ' ':
+                        if (inSingleQuotes || inDoubleQuotes) {
+                            currentArg.append(current);
+                        } else if (!currentArg.isEmpty()) {
+                            argsList.add(currentArg.toString());
+                            currentArg = new StringBuilder();
+                        }
+                        break;
+                    case '"':
+                        if (inSingleQuotes) {
+                            currentArg.append(current);
+                        } else {
+                            inDoubleQuotes = !inDoubleQuotes;
+                        }
+                        break;
+                    case '\\':
+                        if (inSingleQuotes) {
+                            currentArg.append(current);
+                        } else if (inDoubleQuotes) {
+                            isEscapedConditionally = true;
+                        } else {
+                            isEscaped = true;
+                        }
+                        break;
+                    default:
+                        currentArg.append(current);
                 }
             }
-            case "pwd" -> out.println(System.getProperty("user.dir"));
-            case "cd" -> {
-                if (tokens.length == 1)
-                    err.println("cd: missing operand");
-                else if (tokens.length > 2)
-                    err.println("cd: too many arguments");
-                else if (tokens[1].equals("~"))
-                    System.setProperty("user.dir",
-                            System.getenv("HOME"));
-                else {
-                    var currentPath = Paths.get(
-                            System.getProperty("user.dir"));
-                    var newPath = currentPath.resolve(tokens[1])
-                            .normalize();
 
-                    if (Files.exists(newPath))
-                        System.setProperty("user.dir",
-                                newPath.toAbsolutePath().toString());
-                    else
-                        err.printf("cd: %s: No such file or directory%n", tokens[1]);
+            argsList.add(currentArg.toString());
+
+            arguments = argsList.toArray(new String[0]);
+
+            String[] execArgs = Arrays.copyOfRange(arguments, 1, arguments.length);
+
+            Optional<Command> command = commandFactory.getCommandByName(arguments[0]);
+
+            OutputStream outStd = System.out;
+            OutputStream outErr = System.err;
+
+            if (argsList.size() > 1) {
+                String secondToLastArg = argsList.get(argsList.size() - 2);
+
+                if (">".equals(secondToLastArg)
+                        || "1>".equals(secondToLastArg)
+                        || ">>".equals(secondToLastArg)
+                        || "1>>".equals(secondToLastArg)) {
+                    Path outputFilePath = Path.of(argsList.getLast());
+
+                    boolean append = ">>".equals(secondToLastArg) || "1>>".equals(secondToLastArg);
+
+                    if (!outputFilePath.isAbsolute()) {
+                        outputFilePath = Path.of(
+                                CurrentPath.INSTANCE.getPath().toString(),
+                                argsList.getLast());
+                    }
+
+                    if (!Files.exists(outputFilePath)) {
+                        Files.createFile(outputFilePath);
+                    }
+
+                    outStd = new FileOutputStream(outputFilePath.toFile(), append);
+
+                    execArgs = Arrays.copyOfRange(arguments, 1, arguments.length - 2);
+                }
+
+                if ("2>".equals(secondToLastArg)) {
+                    Path outputFilePath = Path.of(argsList.getLast());
+
+                    if (!outputFilePath.isAbsolute()) {
+                        outputFilePath = Path.of(
+                                CurrentPath.INSTANCE.getPath().toString(),
+                                argsList.getLast());
+                    }
+
+                    if (!Files.exists(outputFilePath)) {
+                        Files.createFile(outputFilePath);
+                    }
+
+                    outErr = new FileOutputStream(outputFilePath.toFile());
+
+                    execArgs = Arrays.copyOfRange(arguments, 1, arguments.length - 2);
                 }
             }
-            case "type" -> {
-                if (tokens.length == 1)
-                    err.println("type: missing operand");
-                else if (tokens.length > 2)
-                    err.println("type: too many arguments");
-                else if (BUILTINS.contains(tokens[1]))
-                    out.println(tokens[1] + " is a shell builtin");
-                else
-                    getFile(tokens[1]).ifPresentOrElse(
-                            f -> out.println(tokens[1]
-                                    + " is " + f.getAbsolutePath()),
-                            () -> err.println(tokens[1] + ": not found"));
 
+            if (command.isEmpty()) {
+                System.err.printf("%s: command not found%n", arguments[0]);
+            } else if (command.get() instanceof Exit) {
+                run = false;
+            } else {
+                CommandOutput output = command.get().execute(execArgs);
+
+                try (InputStream std = output.standard()) {
+                    std.transferTo(outStd);
+                } finally {
+                    if (!outStd.equals(System.out)) {
+                        outStd.close();
+                    }
+                }
+
+                try (InputStream err = output.error()) {
+                    err.transferTo(outErr);
+                } finally {
+                    if (!outErr.equals(System.err)) {
+                        outErr.close();
+                    }
+                }
             }
-            default -> getFile(tokens[0]).ifPresentOrElse(
-                    _ -> runProgram(tokens, out, err),
-                    () -> err.println(tokens[0] + ": command not found"));
         }
-        out.flush();
-        err.flush();
-        return new Output(outStream.toString(), errStream.toString());
-    } catch (IOException e) {
-        e.printStackTrace(System.err);
-        return null;
-    }
-}
-
-Optional<File> getFile(String token) {
-    Objects.requireNonNull(token);
-
-    final String PATH = System.getenv("PATH");
-    assert PATH != null : "PATH can't be null";
-
-    // Check if PATH contains the file
-    for (final String filePath : PATH.split(File.pathSeparator)) {
-        final var file = new File(
-                filePath + File.separator + token);
-        if (file.exists() && file.canExecute())
-            return Optional.of(file);
-    }
-    return Optional.empty();
-}
-
-void runProgram(String[] tokens, PrintStream out, PrintStream err) {
-    assert tokens != null;
-    assert tokens.length > 0;
-
-    try {
-        var process = Runtime.getRuntime().exec(tokens);
-        process.getInputStream().transferTo(out);
-        process.getErrorStream().transferTo(err);
-    } catch (IOException e) {
-        err.println("An I/O error occurred: " + e.getMessage());
-    } catch (Exception e) {
-        e.printStackTrace(err);
-    }
-}
-
-String[] tokenize(String s) {
-    var tokens = new ArrayList<String>();
-
-    var sb = new StringBuilder();
-    boolean inSingleQuote = false, inDoubleQuote = false;
-
-    int i = 0;
-    while (i < s.length()) {
-        final char c = s.charAt(i);
-        if (c == '\\' && i + 1 < s.length())
-            sb.append(inSingleQuote ? c
-                    : s.charAt(++i));
-        else if (c == '\'' && !inDoubleQuote)
-            inSingleQuote = !inSingleQuote;
-        else if (c == '"' && !inSingleQuote) {
-            if (inDoubleQuote && i + 1 < s.length() && s.charAt(i + 1) == '"') {
-                // Skip the next character
-                i++;
-                continue;
-            }
-            inDoubleQuote = !inDoubleQuote;
-        } else if (!inSingleQuote && !inDoubleQuote
-                && Character.isWhitespace(c)) {
-            if (!sb.isEmpty()) {
-                tokens.add(sb.toString());
-                sb.setLength(0);
-            }
-        } else {
-            sb.append(c);
-        }
-        i++;
-    }
-
-    if (!sb.isEmpty())
-        tokens.add(sb.toString());
-    return tokens.toArray(String[]::new);
-}
-
-record Output(String std, String err) {
-    Output {
-        Objects.requireNonNull(std);
-        Objects.requireNonNull(err);
     }
 }
