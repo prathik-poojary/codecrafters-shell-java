eb255bf440a0ccf1ddc1eee7912f006911bd52b7
prathik-poojary <prathikpoojary359@gmail.com>
2026-06-20 21:52:06 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public final class Parser {
+public final class Main {
     public static List<String> parseArgs(String input) {
         final Pattern commandPattern = Pattern.compile("^\\s*(\\S+)");
 
