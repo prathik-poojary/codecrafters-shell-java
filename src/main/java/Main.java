2c7f89149b0968570ba79bac9d2199e3b1fdc32d
prathik-poojary <prathikpoojary359@gmail.com>
2026-06-20 19:31:00 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,17 @@
 import java.io.File;
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        String path = System.getenv("PATH");
-        String[] pathDirs = path.split(":");
+private static String getPathToExecutable(String command) {
+    String pathEnv = System.getenv("PATH");
+    if (pathEnv == null)
+        return null;
 
-        while (true) {
-            System.out.print("$ ");
-            String command = sc.nextLine();
-
-            if (command.equals("exit")) {
-                break;
-            } else if (command.startsWith("echo")) {
-                System.out.println(command.substring(5));
-            } else if (command.startsWith("type")) {
-                String typeArg = command.substring(5);
-                System.out.println(type(typeArg));
-            } else {
-                System.out.println(command + ": command not found");
-            }
+    // PATH directories are separated by ':' on Unix/Linux
+    String[] directories = pathEnv.split(":");
+    for (String dir : directories) {
+        File file = new File(dir, command);
+        if (file.exists() && file.canExecute()) {
+            return file.getAbsolutePath();
         }
-
-        sc.close();
     }
-
-    public static String type(String command) {
-        String[] commands = { "exit", "echo", "type" };
-        String path = System.getenv("PATH");
-        String[] pathDirs = path.split(":");
-
-        boolean isBuiltIn = false;
-        for (int i = 0; i < commands.length; i++) {
-            if (commands[i].equals(command)) {
-                return command + " is a shell builtin";
-            }
-        }
-
-        for (int i = 0; i < pathDirs.length; i++) {
-            File file = new File(pathDirs[i], command);
-            if (file.exists() && file.canExecute()) {
-                return command + " is " + file.getAbsolutePath();
-            }
-        }
-
-        return command + ": not found";
-    }
+    return null;
 }
