82d6bfd1f510c89bc6e464d9b73673f727c784ce
prathik-poojary <prathikpoojary359@gmail.com>
2026-06-20 19:26:50 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.print("$ ");
-            String input = scanner.nextLine();
-            if (input.equals("exit")) {
+
+            String input = sc.nextLine();
+
+            String cmd = input.indexOf(" ") == -1 ? input : input.substring(0, input.indexOf(" "));
+            String rem = input.indexOf(" ") == -1 ? "" : input.substring(input.indexOf(" ") + 1);
+
+            if (cmd.equals("exit")) {
                 break;
-            } else if (input.startsWith("echo ")) {
-                System.out.println(input.substring(5));
+            } else if (cmd.equals("type")) {
+                if (rem.equals("exit") || rem.equals("echo") || rem.equals("type"))
+                    System.out.println(rem + " is a shell builtin");
+                else
+                    System.out.println(rem + ": not found");
+            } else if (cmd.equals("echo")) {
+                System.out.println(rem);
             } else {
-                System.out.println(input + ": command not found");
+                System.out.println(cmd + ": command not found");
             }
         }
     }
